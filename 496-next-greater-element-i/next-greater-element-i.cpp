3d3b20b0d095b91785a6e397e265473add21b22e
Chinmay Singhal <byterogue16x@gmail.com>
2026-07-29 00:13:32 +0530

    Time: 11 ms (5.89%) | Memory: 12.1 MB (97.18%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -7,17 +7,16 @@ public:
             int max = -1;
             for(int j = 0; j < nums2.size() ; j++)
             {
-                int index = j ;
                 if(nums1[i] == nums2[j])
                 {
-                    while(index != nums2.size())
+                    while(j != nums2.size())
                     {
-                        if(nums2[index] > nums1[i])
+                        if(nums2[j] > nums1[i])
                         {
-                            max = nums2[index] ;
+                            max = nums2[j] ;
                             break ;
                         }
-                        index++ ;
+                        j++ ;
                     }
                 }
             }
@@ -26,3 +25,44 @@ public:
         return ans ;
     }
 };
+
+
+
+/*
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        stack<int> s;
+        unordered_map<int,int> mp;
+
+        for(int i = nums2.size() - 1; i >= 0; i--)
+        {
+            while(!s.empty() && nums2[i] >= s.top())
+            {
+                s.pop();
+            }
+
+            if(s.empty())
+            {
+                mp[nums2[i]] = -1;
+            }
+            else
+            {
+                mp[nums2[i]] = s.top();
+            }
+
+            s.push(nums2[i]);
+        }
+
+        vector<int> ans;
+
+        for(int x : nums1)
+        {
+            ans.push_back(mp[x]);
+        }
+
+        return ans;
+    }
+};
+*/ 
