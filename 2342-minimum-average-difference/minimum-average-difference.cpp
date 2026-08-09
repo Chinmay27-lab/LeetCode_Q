d384ac750563ba9181936bb6df9bcd4a003049a5
Chinmay Singhal <byterogue16x@gmail.com>
2026-08-09 12:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 82.1 MB (77.08%) - LeetSync

---
+++ 2342-minimum-average-difference/minimum-average-difference.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+
+        int n = nums.size();
+
+        long long totalSum = 0;
+
+        for(int x : nums) {
+            totalSum += x;
+        }
+
+        long long leftSum = 0;
+
+        int ans = 0;
+        long long minDiff = LLONG_MAX;
+
+        for(int i = 0; i < n; i++) {
+
+            leftSum += nums[i];
+
+            long long leftAvg = leftSum / (i + 1);
+
+            long long rightSum = totalSum - leftSum;
+
+            long long rightAvg;
+
+            if(i == n - 1)
+                rightAvg = 0;
+            else
+                rightAvg = rightSum / (n - i - 1);
+
+            long long diff = abs(leftAvg - rightAvg);
+
+            if(diff < minDiff) {
+                minDiff = diff;
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+};
