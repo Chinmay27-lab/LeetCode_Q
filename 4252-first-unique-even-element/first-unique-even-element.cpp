26a887a880d6b4ce2ffdfa7c8db92760918d1b77
Chinmay Singhal <byterogue16x@gmail.com>
2026-08-29 14:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (74.38%) - LeetSync

---
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        vector<int>freq(101 , 0) ;
+        for(int x : nums)
+        {
+            freq[x]++ ;
+        }
+        for(int x : nums)
+        {
+            if(x%2 == 0 && freq[x] == 1)
+            return x ;
+        }
+        return -1 ;
+    }
+};
