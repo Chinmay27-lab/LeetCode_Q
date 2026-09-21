afd7fec44225d576f2496b7e9cfbb4da72c0c986
Chinmay Singhal <byterogue16x@gmail.com>
2026-09-21 16:24:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.57%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
     int countCommas(int n) {
+        int sum = 0;
+        for(int i = 1000 ; i <=n ;i++)
+        {
+            sum += 1 ;
+        }
+        return sum ;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    int countCommas(int n) {
         if(n < 1000) return 0 ;
         if(n == 100000) return (100000 - 999) ;
         return n - 999 ;
     }
 };
+*/ 
